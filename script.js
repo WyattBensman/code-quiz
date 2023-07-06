546428e808a08aad32d841231df7d0bbfc49017f
WyattBensman <wyattbensman5@gmail.com>
2023-07-05 23:39:59 -0400

    Added Event Listeners for Navigation throughout the quiz

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+// Section References
+const introSection = document.querySelector('#introSection');
+const questionSection = document.querySelector('#questionSection');
+const doneSection = document.querySelector('#doneSection');
+const highScoresSection = document.querySelector('#highScoresSection');
+
+const introBtn = document.querySelector('#introBtn');
+const highScores = document.querySelector('.navbar-brand')
+const goBackBtn = document.querySelector('#goBack')
+
+// Start Quiz
+introBtn.addEventListener("click", () => {
+    introSection.style.display = "none";
+    questionSection.style.display = "flex";
+    setTime();
+});
+
+// View High Scores
+highScores.addEventListener("click", () => {
+    introSection.style.display = "none";
+    questionSection.style.display = "none";
+    doneSection.style.display = "none";
+    highScoresSection.style.display = "flex";
+})
+
+// Go Back in High Scores
+goBackBtn.addEventListener("click", () => {
+    highScoresSection.style.display = "none";
+    introSection.style.display = "flex";
+})
+
+
+
+// Time Interval
 let timeElement = document.querySelector('#time')
 
 let secondsLeft = 76;
@@ -7,7 +41,7 @@ function setTime() {
         secondsLeft--;
         timeElement.textContent = `Time: ${secondsLeft}`;
 
-        if (secondsLeft === 0) {
+        if (secondsLeft <= 0) {
             // Stops execution of action at set interval
             clearInterval(timerInterval);
         }
